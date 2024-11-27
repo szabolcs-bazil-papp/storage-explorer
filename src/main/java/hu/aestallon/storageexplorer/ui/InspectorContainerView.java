c2e0f7b4b7dab357e6e89b3faf7c0b62dd8cce60
Szabolcs Bazil Papp <szabolcs.papp@it4all.hu>
2024-11-27 18:06:16 +0100

    remove duplicate call

--- src/main/java/hu/aestallon/storageexplorer/ui/InspectorContainerView.java
+++ src/main/java/hu/aestallon/storageexplorer/ui/InspectorContainerView.java
@@ -152,7 +152,6 @@ public class InspectorContainerView extends JTabbedPane {
     @Override
     protected void paintComponent(Graphics g) {
       super.paintComponent(g);
-      super.paintComponent(g);
       Graphics2D g2 = (Graphics2D) g.create();
       //shift the image for pressed buttons
       if (getModel().isPressed()) {
