f571f65c96d6e38d2d60e85a4f1ac9264646a641
Szabolcs Bazil Papp <szabolcs.papp@it4all.hu>
2024-12-27 12:38:53 +0100

    amend demo app to use new package name, bump starter version to match the local playground version

--- app-demo/src/main/java/com/example/DemoApplication.java
+++ app-demo/src/main/java/com/example/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import hu.aestallon.storageexplorer.springstarter.EnableStorageExplorer;
+import com.aestallon.storageexplorer.springstarter.EnableStorageExplorer;
 
 @SpringBootApplication(exclude = { ErrorMvcAutoConfiguration.class })
 @EnableStorageExplorer
