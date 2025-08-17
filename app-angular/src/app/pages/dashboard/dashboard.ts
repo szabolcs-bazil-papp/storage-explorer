f78b3e2135b2b1c8381da0b210396e94ff30142a
Szabolcs Bazil Papp <szabolcs.papp@it4all.hu>
2025-08-17 22:22:53 +0200

    [#107] add basic tutorial text to the so-called Dashboard

--- app-angular/src/app/pages/dashboard/dashboard.ts
+++ app-angular/src/app/pages/dashboard/dashboard.ts
@@ -14,11 +14,77 @@
  */
 
 import {Component} from '@angular/core';
+import {Fieldset} from 'primeng/fieldset';
+import {ScrollPanel} from 'primeng/scrollpanel';
+import {Avatar} from 'primeng/avatar';
 
 @Component({
   selector: 'dashboard',
+  imports: [
+    Fieldset,
+    ScrollPanel,
+    Avatar
+  ],
   template: `
-    <p>Hello World From Dashbboard!</p>`
+    <p-scroll-panel
+      [style]="{ height: '100%', width: '100%', 'padding-left': '1rem', 'padding-right': '1rem'}">
+      <p-fieldset>
+        <ng-template #header>
+          <h2>Dashboard</h2>
+        </ng-template>
+        <h3>Query the Storage</h3>
+        <p>Using the
+          <p-avatar icon="pi pi-code" shape="circle"></p-avatar>
+          <b>ArcScript</b> page, issue queries to the storage. You can find documentation on how to
+          use the query language <a
+            href="https://github.com/szabolcs-bazil-papp/storage-explorer/wiki/ArcScript">on the
+            wiki.</a>
+        </p>
+        <p>Queries may be executed using the <code>Ctrl+Enter</code> hotkey.</p>
+        <h3>Inspect entries</h3>
+        <p>Double clicking any row in your query results will load the data associated with the
+          entry in an
+          <p-avatar icon="pi pi-compass" shape="circle"></p-avatar>
+          <b>Inspector</b>.
+        </p>
+        <p>You may directly load an exact, versionless <code>URI</code> by clicking the
+          <p-avatar icon="pi pi-spinner-dotted" shape="circle"></p-avatar>
+          button in the tree, or using the <code>Control+Shift+L</code> hotkey.
+        </p>
+        <p>If you have many
+          <p-avatar icon="pi pi-compass" shape="circle"></p-avatar>
+          <b>Inspector</b>s open, you can search and filter them by clicking the
+          <p-avatar icon="pi pi-search" shape="circle"></p-avatar>
+          button in the nav bar.
+        </p>
+        <i>
+          <h3>Known quirks</h3>
+          <ul>
+            <li>
+              <p>If you specify <code>show</code> commands in a query, you must</p>
+              <ol>
+                <li>Specify the <code>URI</code> property explicitly, such as <code
+                  class="code-block">show 'uri' as
+                  'uri'</code></li>
+                <li>Always specify an alias ( <code class="code-block">as</code> ) for your
+                  properties - these aliases
+                  must not contain dots.
+                </li>
+              </ol>
+            </li>
+            <li>Sometimes the scrollbars on
+              <p-avatar icon="pi pi-compass" shape="circle"></p-avatar>
+              <b>Inspector</b> tabs glitch out and will not appear. This is a side effect of how
+              PrimeNG detects if any tabs are not in the viewport. This will eventually solved when
+              I move away from the PrimeNG component, until then, I'm very sorry!
+            </li>
+          </ul>
+        </i>
+        <h3>What's next?</h3>
+        <p>As you can see, this page is currently <strong>anything but a dashboard</strong>. I
+          plan to prefetch schema and type information and display it here.</p>
+      </p-fieldset>
+    </p-scroll-panel>`
 })
 export class Dashboard {
 
