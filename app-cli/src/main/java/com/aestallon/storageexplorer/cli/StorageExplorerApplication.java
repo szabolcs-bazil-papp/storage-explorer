537afaf54703f5da46912741de3b6fa51c12f20d
Szabolcs Bazil Papp <szabolcs.papp@it4all.hu>
2025-08-22 19:32:08 +0200

    [#62] scaffold CLI module

---
+++ app-cli/src/main/java/com/aestallon/storageexplorer/cli/StorageExplorerApplication.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2025 Szabolcs Bazil Papp
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.aestallon.storageexplorer.cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(
+    exclude = DataSourceAutoConfiguration.class,
+    scanBasePackages = {
+        "com.aestallon.storageexplorer.client",
+    })
+public class StorageExplorerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StorageExplorerApplication.class, args);
+  }
+
+}
